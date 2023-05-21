6cb23bf62de0354539f043c82b82f652215e34b0
Bismark Arroyo <bis.arroyo@gmail.com>
2023-05-21 15:59:26 -0600

    Add localStarage functionality

---
+++ src/hooks/useNotificarions.jsx
@@ -0,0 +1,92 @@
+import {
+  AddTodo,
+  RemoveTodo,
+  RemovePermanentTodo,
+  setEdit,
+  EditTodo,
+  setFilter,
+  ToggleTodo,
+  StarredTodo
+} from '../actions'
+import { useAppContext } from './useAppContext'
+
+export const useTodo = () => {
+  const { todos, dispatch } = useAppContext()
+
+  const handleAddTodo = (todo) => {
+    const action = {
+      type: AddTodo,
+      payload: todo
+    }
+    dispatch(action)
+  }
+
+  const handleRemoveTodo = (id) => {
+    const action = {
+      type: RemoveTodo,
+      payload: id
+    }
+    dispatch(action)
+  }
+  const handleRemoveTodoPermanent = (id) => {
+    const action = {
+      type: RemovePermanentTodo,
+      payload: id
+    }
+    dispatch(action)
+  }
+
+  const setEditing = (todo) => {
+    const action = {
+      type: setEdit,
+      payload: todo
+    }
+    dispatch(action)
+  }
+
+  const handleEditTodo = (id, description) => {
+    const action = {
+      type: EditTodo,
+      payload: {
+        id,
+        description
+      }
+    }
+    dispatch(action)
+  }
+  const handleFilterTodo = (filter) => {
+    const action = {
+      type: setFilter,
+      payload: filter
+    }
+    dispatch(action)
+  }
+
+  const handleToggleTodo = (id) => {
+    const action = {
+      type: ToggleTodo,
+      payload: id
+    }
+    dispatch(action)
+  }
+
+  const handleStarredTodo = (id) => {
+    const action = {
+      type: StarredTodo,
+      payload: id
+    }
+    dispatch(action)
+  }
+
+  return {
+    todos,
+    handleAddTodo,
+    handleRemoveTodo,
+    handleRemoveTodoPermanent,
+    setEditing,
+    handleEditTodo,
+    handleFilterTodo,
+    handleToggleTodo,
+    handleStarredTodo
+  }
+}
